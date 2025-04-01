0fa2de60b303e9b0564cd19e666fc5ca2eccf0c9
Michael <michaelrr72@outlook.com>
2025-04-01 00:27:16 -0500

    Correccion sobre favoritos
    Se mantiene integridad de las listas de favoritos por usuarios y no una lista general

--- Recetas/src/services/ServiceFavoriteRecipe.js
+++ Recetas/src/services/ServiceFavoriteRecipe.js
@@ -1,27 +1,39 @@
-import { db } from './firebaseConfig';
+import { db, auth } from './firebaseConfig';
 import { ref, set, remove, get } from "firebase/database";
 
 // Agregar receta a favoritos
 export const addToFavorites = async (meal) => {
-  const mealRef = ref(db, `favorites/${meal.idMeal}`);
+  const user = auth.currentUser;
+  if (!user) throw new Error("El usuario no está autenticado");
+  const userId = user.uid;
+  const mealRef = ref(db, `favorites/${userId}/${meal.idMeal}`);
   await set(mealRef, meal);
 };
 
 // Eliminar receta de favoritos
 export const removeFromFavorites = async (mealId) => {
-  const mealRef = ref(db, `favorites/${mealId}`);
+  const user = auth.currentUser;
+  if (!user) throw new Error("El usuario no está autenticado");
+  const userId = user.uid;
+  const mealRef = ref(db, `favorites/${userId}/${mealId}`);
   await remove(mealRef);
 };
 
 // Verificar si está en favoritos
 export const isFavorite = async (mealId) => {
-  const mealRef = ref(db, `favorites/${mealId}`);
+  const user = auth.currentUser;
+  if (!user) return false;
+  const userId = user.uid;
+  const mealRef = ref(db, `favorites/${userId}/${mealId}`);
   const snapshot = await get(mealRef);
   return snapshot.exists();
 };
 
 //traer la lista de favoritos
 export const getFavorites = async () => {
-  const snapshot = await get(ref(db, "favorites"));
+  const user = auth.currentUser;
+  if (!user) throw new Error("El usuario no está autenticado");
+  const userId = user.uid;
+  const snapshot = await get(ref(db, `favorites/${userId}`));
   return snapshot.exists() ? Object.values(snapshot.val()) : [];
 };
